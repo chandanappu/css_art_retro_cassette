ed86cb328a340b451fd4edd2ce4268bb56cbc971
chandanappu <137379695+chandanappu@users.noreply.github.com>
2024-05-28 16:41:50 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='camera'> 
+    <div className='black_body'>
+      <div className='white_body'>
+        <hr className='hr hr_top'/>
+        <hr className=' hr hr_buttom'/>
+        <div className='colors'>
+        <hr className='hr_color hr_red'></hr>
+        <hr className='hr_color hr_yellow'></hr>
+        <hr className='hr_color hr_green'></hr>
+        <hr className='hr_color hr_blue'></hr>
+        <hr className='hr_color hr_orange'></hr>
+        </div>
+        
+        <div className='inner_body'>
+          
+          <div className='circle circle_1'></div>
+
+          <div className='shutter'>
+            <div className='mirror'></div>
+          </div>
+          <div className='circle circle_2'></div>
+         
+        </div>
+        
+      </div>
+      <div className='holder'></div>
     </div>
+    
+   
+        </div>
   );
 }
 
 export default App;
+
+  
